0d7dacfdb2f3e2c9ea32473fd577953af5d4bbe9
Altrim Havolli <ah40162@ubt-uni.net>
2021-04-29 20:50:28 +0200

    added collection overview

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,31 +1,13 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data.js'
-import CollectionPreview from '../../components/collection-preview/CollectionPreview';
+import CollectionsOverview from '../../components/collections-overview/collections-overview';
 
 import './Shop.scss';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+const ShopPage= ({ collections }) => (
+    <div className="shop-page">
+        <CollectionsOverview />
+    </div>
+);
 
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
-    
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({ id, ...otherCollectionProps }) => (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-        )
-    }
-}
-
-export default ShopPage
+export default ShopPage;
